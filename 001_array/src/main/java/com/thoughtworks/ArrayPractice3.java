131f8043a4adbeafcf84e8b7ea7adc633cbb6aef
lpdft <772346730@qq.com>
2020-02-03 15:55:36 +0800

    [ruyue] feat-pratice3: filter 0 get new array

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -6,7 +6,19 @@ public class ArrayPractice3 {
      * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
      */
     public static int[] filterZero(int[] array) {
-
-        return null;
+        int len = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] != 0) {
+                ++len;
+            }
+        }
+        int[] result = new int[len];
+        int index = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] != 0) {
+                result[index++] = array[i];
+            }
+        }
+        return result;
     }
 }
